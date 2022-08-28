9835a00bf359498b1b7d680bd8bfd9b8489c6caa
Cloudness354 <youseftarek354@gmail.com>
2022-08-28 13:49:01 +0200

    Adding serverless error logging

--- services/util/handler.js
+++ services/util/handler.js
@@ -1,13 +1,20 @@
+import * as debug from "./debug";
+
 export default function handler(lambda) {
   return async function (event, context) {
     let body, statusCode;
+
+    // Start debugger
+    debug.init(event);
 
     try {
       // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
-      console.error(e);
+      // Print debug messages
+      debug.flush(e);
+
       body = { error: e.message };
       statusCode = 500;
     }
@@ -16,6 +23,10 @@ export default function handler(lambda) {
     return {
       statusCode,
       body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   };
 }
